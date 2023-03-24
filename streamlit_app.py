9f1df906a167aab7233a7ef898a88de09e61df13
tdomanski <01150679@pw.edu.pl>
2023-03-24 18:47:54 +0100

    make_subplots modification

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,7 +149,7 @@ else:
             rand_value = random.randint(1000, signal.shape[0]-1000)
             rand_range = (rand_value, rand_value+1000)
             # fig = go.Figure()
-            fig = make_subplots(rows=12, cols=1)
+            fig = make_subplots(rows=12, cols=1, shared_xaxes=True, vertical_spacing=0.05)
             for i in range(12):
                 signal_name = rec[1]['sig_name'][i]
                 signal = rec[0][:, i]
